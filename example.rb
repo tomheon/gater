834aa176e51d967d7be08d98339398fad7d62188
Greg Belote <etoleb@gmail.com>
2011-02-19 16:51:26 -0500

    improved example, included default branch

--- example.rb
+++ example.rb
@@ -6,26 +6,19 @@ g.junction do |switcher|
     switcher.branch :feature1 do |reporter|
         puts "Feature 1 is active"
     end
+
+    switcher.branch do |reporter|
+        puts "Feature 1 isn't active"
+    end
 end
 
 g.junction do |switcher|
     switcher.branch :feature2 do |reporter|
         puts "Feature 2 is active"
     end
-end
 
-g.junction do |switcher|
     switcher.branch :feature3 do |reporter|
         puts "Feature 3 is active"
-    end
-end
-
-# TODO: examples for mutex logic
-
-# Do something special when both feature2 and feature3 are active
-g.junction do |switcher|
-    switcher.branch :feature2 do |reporter|
-        puts "Only Feature 2 is active, all is well"
     end
 
     switcher.branch [:feature2, :feature3] do |reporter|
@@ -33,5 +26,4 @@ g.junction do |switcher|
     end
 end
 
-# TODO: examples for a default branch
 
